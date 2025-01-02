63dfb08b59a96fabec5634ac37fe784e11c2c134
hallowoo <164675169+hallowoo@users.noreply.github.com>
2025-01-03 00:06:35 +0900

    final commit

--- work3/work3addtional.cpp
+++ work3/work3addtional.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
@@ -11,6 +12,18 @@ private:
 	int currentSize;
 	int currentCapacity;
 
+	void resize(int newCapacity) {
+		newCapacity = currentCapacity + 5;
+		T* newData = new T[newCapacity];
+		for (int i = 0; i < currentSize; i++) {
+			newData[i] = data[i];
+		}
+		delete[] data;
+		data = newData;
+		currentCapacity = newCapacity;
+		
+	}
+
 
 public:
 
@@ -22,15 +35,28 @@ public:
 		data = new T[currentCapacity];
 	}
 
+	SimpleVector(const SimpleVector& other) : currentSize(other.currentSize), currentCapacity(other.currentCapacity){
+		data = new T[other.currentCapacity];
+		for (int i = 0; i < currentSize; i++) {
+			data[i] = other.data[i];
+		}
+	
+	}
+
 	~SimpleVector() {
+		cout << "메모리 해제" << endl;
 		delete[] data;
 	}
 
 	void push_back(const T& value) {
-		if (currentSize < currentCapacity) {
+		if (currentSize >= currentCapacity) {
+			currentSize = currentCapacity;
+			resize(currentCapacity + 5);
 			data[currentSize++] = value;
 		}
-		else {}
+		else {
+			data[currentSize++] = value;
+		}
 	}
 
 	void pop_back() {
@@ -49,51 +75,77 @@ public:
 		return currentCapacity;
 	}
 
-	string printData() {
-		string s;
+	void printData() {		
 
 		for (int i = 0; i < currentSize; i++) {
-			s += data[i];
-		}
-		return s;
-
+			cout<< data[i];
+		}		
+		cout << "\n";
 	}
 
+	void sortData() {
+		sort(data, data+currentSize);
+		cout << "정렬 : ";
+		for (int i = 0; i < currentSize; i++) {
+			cout << data[i];
+		}
+		cout << "\n";
+	}
 };
 
 
 
 int main() {
 
-	SimpleVector<int>vec;
+	SimpleVector<int>vec; // 디폴트 생성
 	cout << "vecCapacity : " << vec.capacity() << endl;
 	cout << "vecsize : " << vec.size() << endl;
 
-	SimpleVector<char>alphabet(5);
+	for (int i = 0; i < vec.capacity(); i++) {
+		vec.push_back(i);
+	}
+	cout << "vec에 배열값이 채워졌습니다." << endl;
+	cout << "vecsize : " << vec.size() << endl;
+	
+	cout << "vec value : ";
+	vec.printData();
+
+
+	SimpleVector<char>alphabet(5); // 크기 설정
 	cout << "alphabetCapacity : " << alphabet.capacity() << endl;
 
-	alphabet.push_back('a');
 	alphabet.push_back('b');
+	alphabet.push_back('a');
 	alphabet.push_back('d');
 
-	cout << "alphabet : " << alphabet.printData() << endl;
+	cout << "alphabet : ";
+	alphabet.printData();
 	cout << "alphabetSize : " << alphabet.size() << endl;
 
 	alphabet.pop_back();
 
-	cout << "delete 'd' from alphabet : " << alphabet.printData() << endl;
+	cout << "delete 'd' from alphabet : "; 
+	alphabet.printData();
 	cout << "alphabetSize : " << alphabet.size() << endl;
-	alphabet.push_back('c');
+	alphabet.push_back('f');
 
-	cout << "add 'c' to alphabet : " << alphabet.printData() << endl;
+	cout << "add 'f' to alphabet : ";
+	alphabet.printData();
 	cout << "alphabetSize : " << alphabet.size() << endl;
 
 	alphabet.push_back('d');
+	alphabet.push_back('c');
 	alphabet.push_back('e');
-	alphabet.push_back('f');
-	cout << "add d,e,f to alphabet : " << alphabet.printData() << endl;
-	cout << "alphabetSize : " << alphabet.size() << endl;
+	cout << "add d,c,e to alphabet : ";
+	alphabet.printData();
+    cout << "alphabetSize : " << alphabet.size() << endl;
+	cout << "alphabetNewCapacity : " << alphabet.capacity() << endl;
 
+	alphabet.sortData();
+
+	SimpleVector<char>alphabet2 = alphabet; //복사 생성
+	cout << "alphabet2 : ";
+	alphabet2.printData();
 
 	return 0;
 }
